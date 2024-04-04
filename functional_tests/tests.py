60311297a1f2825c1bd4ae737379e27beaff9fdd
Sumit Kothari <sumitkam@atrihub.io>
2024-04-04 10:33:39 -0700

    Training(Learning TDD): TDD Tutorial Chapter 6 Code Implementation of Chapter6 of TDD Tutorial
    [1651]

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,12 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+from django.test import LiveServerTestCase
+
 import json
+
+MAX_WAIT = 5
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -26,6 +31,19 @@ class NewVisitorTest(unittest.TestCase):
     #     [...]
 
         # Satisfied, she goes back to sleep
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
     def test_can_vote_on_question(self):
         # User opens the app
         self.browser.get("http://localhost:8000")
@@ -52,10 +70,10 @@ class NewVisitorTest(unittest.TestCase):
 
         self.assertIn(question_text, voted_items_texts)
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    # def check_for_row_in_list_table(self, row_text):
+    #     table = self.browser.find_element(By.ID, "id_list_table")
+    #     rows = table.find_elements(By.TAG_NAME, "tr")
+    #     self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
@@ -78,8 +96,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
@@ -87,11 +104,11 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
 
 
 class UsersAPITest(unittest.TestCase):
